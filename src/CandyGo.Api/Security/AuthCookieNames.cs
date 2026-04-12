2030ebf8c6b6d8613b1e476638f917102157a68a
rodolfo <judasrvg@gmail.com>
2026-04-12 10:33:33 -0400

    fix revert

--- src/CandyGo.Api/Security/AuthCookieNames.cs
+++
@@ -1,7 +0,0 @@
-namespace CandyGo.Api.Security;
-
-public static class AuthCookieNames
-{
-    public const string ClientAccessToken = "cg_client_jwt";
-    public const string AdminAccessToken = "cg_admin_jwt";
-}
